dbc31b4b54ef28e1f322dd861813ffa7b44a8601
N42 <quentin.lieumont@gmail.com>
2018-10-10 17:43:33 +0200

    nouveau data.py allez lire

--- data.py
+++ data.py
@@ -1,11 +1,39 @@
+"""
+
+MaxXP   : tableau 1D    : XP maximum par UE     : [max1,max2,...]
+XP      : tableau 2D    : XP par UE par eleve   : [[e1,xp11,xp12,...],[e2,xp21,xp22,...]]
+
+
+
+"""
+# imports ----------------------------------------------------------------------
 import csv
+
+
+#-------------------------------------------------------------------------------
+
+
 data = list(csv.reader(open('./donnee.txt', 'r')))
 
+#-------------------------------------------------------------------------------
+# supression entete dans les donnees + formatage xp maximum par UE
+
 entete = data.pop(0)
-entete = [int(float(entete[i])) for i in range(len(entete)-1)] + [entete[len(entete)-1]]
+MaxXP = [int(float(entete[i])) for i in range(len(entete)-1)]
+
+
+#-------------------------------------------------------------------------------
+# formatage des donnees en int
 
 data = [list(map(lambda x: int(float(x)),d)) for d in data]
+
+#-------------------------------------------------------------------------------
+# creation des xp de chaques etudiants
+
 xp = [[i] + data[i][:-1] for i in range(len(data))]
+
+#-------------------------------------------------------------------------------
+# affichage si le prg est executé seul (ne pas en tenir compte)
 
 
 if __name__ == "__main__":
