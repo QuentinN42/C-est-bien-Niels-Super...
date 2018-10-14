aba467796ab80fb94e4d30d9caf87533d8d72733
N42 <quentin.lieumont@gmail.com>
2018-10-14 22:29:42 +0200

    Deux comantaires

--- mickael.py
+++ mickael.py
@@ -14,8 +14,9 @@ print(entete)
 def moy(l):
     return sum(l)/len(l)
 
-def ecart_type(l):
+def ecart_type(l): 
     e=0
+    # tu peut calculer la moyenne ici tu la calcule une seule fois donc ca vas plus vite :)
     for i in l:
         e+=(i-moy(l))**2
     return np.sqrt(e/len(l))
@@ -23,3 +24,4 @@ def ecart_type(l):
 print(ecart_type([5,6,7,2,1,9,8,7,5,2,3,2,8]))
 
 def quartiles(l):
+    return None # toujours metre un truc dans une fonction (meme si c'est rien :)
