8486c7541a23c90b9a777afa0933a8db5038ff95
N42 <quentin.lieumont@gmail.com>
2018-11-01 22:47:38 +0100

    Site ajouté
    index.php

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+		<link rel="shortcut icon" type="image/png" href="img/logo.png"/>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<meta http-equiv= "content-type" content= "text/html; charset=UTF-8" >
+        <title> Institut Villebon Charpak </title>
+        <script src="index.js"></script>
+    </head>
+
+    <body onresize="changeHeight()" onload="changeHeight()" onclick="hidemain()">
+        <div class="main" id="main">
+            <img class="bg" src="img/bg.jpg" alt="Institut Villebon Charpak">
+                <div class="title">
+                    <h1>
+                        Institut Villebon Charpak
+                    </h1>
+                    <h2>
+                        Notations
+                    </h2>
+                </div>
+        </div>
+
+
+        <div>
+            <h2>
+                Présentation de l'entreprise
+            </h2>
+            <p>
+                <br>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                <center>
+                    <code data-claire-semantic="text" style="border: 1px solid black;padding-left:5px;padding-right:5px;"> 15dz5KE7EcwaJuyKiFWsusZrPscEeziS8K  </code>
+                </center>
+            </p>
+            <br><br><br><br><br><br>
+        </div>
+
+    </body>
+</html>
